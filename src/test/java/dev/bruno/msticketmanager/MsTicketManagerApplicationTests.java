42983824caf2966c37f69bef8acf2a98a55433b6
Bruno <brunobaldiga1@gmail.com>
2025-01-23 14:42:16 -0300

    feat: Implementação da base do microsserviço ms-ticket-manager

---
+++ src/test/java/dev/bruno/msticketmanager/MsTicketManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bruno.msticketmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTicketManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
