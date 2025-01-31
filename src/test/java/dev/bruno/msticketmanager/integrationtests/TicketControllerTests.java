5fbf6e32abbdfbdaa53980889a67d5bcaedb01ee
Bruno <brunobaldiga1@gmail.com>
2025-01-31 13:52:23 -0300

    feat: Implementação do Docker

--- src/test/java/dev/bruno/msticketmanager/integrationtests/TicketControllerTests.java
+++ src/test/java/dev/bruno/msticketmanager/integrationtests/TicketControllerTests.java
@@ -24,7 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(TicketController.class)
-public class TicketControllerTests {
+public class EventControllerTests {
     @Autowired
     private MockMvc mockMvc;
 
